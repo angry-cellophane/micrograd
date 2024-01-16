f77b298411d524efca5386d90b009684039fe8ed
Aleksandr Kazakov <alexkazakov@fb.com>
2024-01-16 00:56:32 +0000

    add graphviz

--- notebook.py
+++ notebook.py
@@ -2,8 +2,11 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-from micrograd.engine import Value
+from micrograd.engine import draw_dot, Value
 
 a = Value(2.0)
 b = Value(-3.0)
-print(a+b)
+c = Value(10.0)
+d = a*b + c
+print(d)
+draw_dot(d)
